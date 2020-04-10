a25629f657e7b76c3fb9b8211eebd3f7f869910c
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-04-10 13:40:16 +0200

    moved all routes into one file and fixed bug with login

--- src/Root.js
+++ src/Root.js
@@ -5,8 +5,19 @@ import {Provider} from 'react-redux';
 import store from 'store';
 import LoadingSpinner from 'components/atoms/LoadingSpinner';
 
+import {routes} from 'routes';
+
+import MenuSidebar from 'components/organisms/MenuSidebar';
+import NextMeet from 'components/organisms/NextMeet';
+import MainTemplate from './components/templates/MainTemplate';
+
 const LoginPage = lazy(() => import('components/pages/LoginPage'));
-const MainRoute = lazy(() => import('components/MainRoute'));
+
+const Home = lazy(() => import('components/pages/HomePage'));
+const Timetable = lazy(() => import('components/pages/SchedulesPage'));
+const TopicDatabase = lazy(() => import('components/pages/TopicDatabasePage'));
+const History = lazy(() => import('components/pages/MeetingHistoryPage'));
+const Account = lazy(() => import('components/pages/AccountPage'));
 
 const Root = (props) => {
   const [user, setUser] = useState(undefined);
@@ -29,11 +40,20 @@ const Root = (props) => {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Suspense fallback={<LoadingSpinner />}>
-            <Switch>
-              <Route path="/" component={MainRoute} />
-            </Switch>
-          </Suspense>
+          <MainTemplate>
+            <MenuSidebar />
+            <NextMeet />
+            <Suspense fallback={<LoadingSpinner />}>
+              <Switch>
+                <Route exact path={routes.home} component={Home} />
+                <Route exact strict path={routes.timetable} component={Timetable} />
+                <Route exact strict path={routes.topicDatabase} component={TopicDatabase} />
+                <Route exact strict path={routes.history} component={History} />
+                <Route exact strict path={routes.account} component={Account} />
+                <Redirect to="/" />
+              </Switch>
+            </Suspense>
+          </MainTemplate>
         </BrowserRouter>
       </Provider>
     );
