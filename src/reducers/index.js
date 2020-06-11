6d03bf6bfbfd4f2bfa82e9c4e5498d91f6e07d5c
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-06-11 16:38:05 +0200

    change reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,125 @@
 import {GET_MEETS_REQUEST, GET_MEETS_SUCCESS, GET_MEETS_FAILURE} from 'actions';
 
+
+
+const _meetingHistory = {
+  pending: false,
+  meetings: [],
+  error: null
+};
+const _schedule = {
+  pending: false,
+  meetings: [],
+  error: null
+};
+const _meetings = {
+  pending: false,
+  meetings: [],
+  error: null
+};
+const _topics = {
+  pending: false,
+  meetings: [],
+  error: null
+};
+
+export function meetingHistoryReducer(state = _meetingHistory, action){
+  switch(action.type) {
+    case INVALIDATE_MEETING_HISTORY:
+      return {
+        ...state,
+        error: true
+      }
+    case REQUEST_MEETING_HISTORY:
+      return {
+        ...state,
+        pending: true,
+        error: false
+      }
+    case RECEIVE_MEETING_HISTORY:
+      return {
+          pending: false,
+          meetings: action.meetings,
+          error: null
+      }
+    default: 
+        return state;
+    };
+};
+export function meetingHistoryReducer(state = _schedule, action){
+  switch(action.type) {
+    case INVALIDATE_SCHEDULE:
+      return {
+        ...state,
+        error: true
+      }
+    case REQUEST_SCHEDULE:
+      return {
+        ...state,
+        pending: true,
+        error: false
+      }
+    case RECEIVE_SCHEDULE:
+      return {
+          pending: false,
+          meetings: action.meetings,
+          error: null
+      }
+    default: 
+        return state;
+    };
+};
+export function meetingHistoryReducer(state = _meetings, action){
+  switch(action.type) {
+    case INVALIDATE_MEETINGS:
+      return {
+        ...state,
+        error: true
+      }
+    case REQUEST_MEETINGS:
+      return {
+        ...state,
+        pending: true,
+        error: false
+      }
+    case RECEIVE_MEETINGS:
+      return {
+          pending: false,
+          meetings: action.meetings,
+          error: null
+      }
+    default: 
+        return state;
+    };
+};
+export function meetingHistoryReducer(state = _meetingHistory, action){
+  switch(action.type) {
+    case INVALIDATE_MEETING_HISTORY:
+      return {
+        ...state,
+        error: true
+      }
+    case REQUEST_MEETING_HISTORY:
+      return {
+        ...state,
+        pending: true,
+        error: false
+      }
+    case RECEIVE_MEETING_HISTORY:
+      return {
+          pending: false,
+          meetings: action.meetings,
+          error: null
+      }
+    default: 
+        return state;
+    };
+};
+
 const initialState = {
   user: {
     pending: true,
     meetings: undefined,
-    error: null,
-  },
-  meetingHistory: {
-    pending: false,
-    meetings: [],
-    error: null,
-  },
-  schedule: {
-    pending: false,
-    meetings: [],
-    error: null,
-  },
-  meetings: {
-    pending: false,
-    meetings: [],
-    error: null,
-  },
-  topics: {
-    pending: false,
-    meetings: [],
     error: null,
   },
   archive: {
