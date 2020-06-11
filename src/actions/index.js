c384420832848014ec06da983c9d34486f4a2511
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-06-11 19:19:00 +0200

    change actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,19 +2,58 @@ export const GET_MEETS_REQUEST = 'GET_MEETS_REQUEST';
 export const GET_MEETS_SUCCESS = 'GET_MEETS_SUCCESS';
 export const GET_MEETS_FAILURE = 'GET_MEETS_FAILURE';
 
-export const fetchMeetsPending = meetType => ({
-  type: GET_MEETS_REQUEST,
-  meetType
+export const fetchMeetingHistoryPending = () => ({
+  type: REQUEST_MEETING_HISTORY
 });
 
-export const fetchMeetsSuccess = (payload, meetType) => ({
+export const fetchMeetingHistorySuccess = meetings => ({
+  type: RECEIVE_MEETING_HISTORY,
+  meetings
+});
+
+export const fetchMeetingHistoryError = error => ({
+  type: INVALIDATE_MEETING_HISTORY,
+  error
+});
+
+export const fetchSchedulePending = () => ({
+  type: REQUEST_SCHEDULE
+});
+
+export const fetchScheduleSuccess = meetings => ({
+  type: RECEIVE_SCHEDULE,
+  meetings
+});
+
+export const fetchScheduleError = error => ({
+  type: INVALIDATE_SCHEDULE,
+  error
+});
+
+export const fetchMeetingsPending = () => ({
+  type: REQUEST_MEETINGS
+});
+
+export const fetchMeetingsSuccess = meetings => ({
+  type: RECEIVE_MEETINGS,
+  meetings
+});
+
+export const fetchMeetingsError = error => ({
+  type: INVALIDATE_MEETINGS,
+  error
+});
+
+export const fetchTopicsPending = () => ({
+  type: INVALIDATE_MEETINGS
+});
+
+export const fetchTopicsSuccess = meetings => ({
   type: GET_MEETS_SUCCESS,
-  payload,
-  meetType
+  meetings
 });
 
-export const fetchMeetsError = (error, meetType) => ({
+export const fetchTopicsError = error => ({
   type: GET_MEETS_FAILURE,
-  error,
-  meetType
+  error
 });
